8e6f14aaf62cde447dae5c64e86980a7a970f7e1
paranoidandroid13 <utf1313@gmail.com>
2020-10-26 03:00:55 +0300

    add git push in deploy.shs

--- deploy.sh
+++ deploy.sh
@@ -15,6 +15,7 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
+git push -f git@github.com:paranoidandroid13/weather-app.git master:gh-pages
 
 # если вы публикуете по адресу https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
